4453d8796f465ad187d4d08c8ed1d93c6fa35ce6
Prasoon Raj <53205955+prasoon902@users.noreply.github.com>
2021-10-07 13:38:52 +0530

    Add files via upload

---
+++ printName.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+class pN:
+    leaves = 12
+    def __init__(self):
+        self.name = "Raj"
+        print("Hello")
+    
+    def printName(self):
+        self.t = "Toll"
+        print("Hello People")
+    
+#     printName()
+
+
+# print(__name__)
+# print("12334")
+
+
+
+if __name__ =="__main__" :
+    print(__name__)
+    print("12334")
+# In[ ]:
+
+
+
+
